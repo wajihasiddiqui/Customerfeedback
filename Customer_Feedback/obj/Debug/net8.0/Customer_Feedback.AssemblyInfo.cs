0d04a3a64ee6b43fa73a4802bd8a22efd2be8f1b
wajihasiddiqui <wajeehasiddiqui333@gmail.com>
2024-11-02 15:54:14 +0500

    ..a

--- Customer_Feedback/obj/Debug/net8.0/Customer_Feedback.AssemblyInfo.cs
+++ Customer_Feedback/obj/Debug/net8.0/Customer_Feedback.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Customer_Feedback")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5101b6fbe53292356902e6924fbc63daa45dc61c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Customer_Feedback")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Customer_Feedback")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
